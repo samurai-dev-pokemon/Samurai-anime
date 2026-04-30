559ede0714a14433fa2f50c305c5faa23a84fd00
samurai-dev-pokemon <samurai.plays2000@gmail.com>
2026-04-30 21:13:41 +0100

    this is better

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "Samurai-anime", // 
+  plugins: [
+    react(),
+    tailwindcss(),
+    // removed viteSingleFile unless you specifically need it
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
